f7d09c21247430e7da15d8c09318a8967de80a75
Mateusz Łassa <mateuszlassa@gmail.com>
2022-05-16 19:37:02 +0200

    Stworzenie szablonu projektu
    Java Spring + Vaadin Designer

---
+++ frontend/maine-ui.ts
@@ -0,0 +1,27 @@
+import { LitElement, html, css, customElement } from 'lit-element';
+import '@vaadin/button/src/vaadin-button.js';
+
+@customElement('maine-ui')
+export class MaineUi extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+          display: block;
+          height: 100%;
+      }
+      `;
+  }
+
+  render() {
+    return html`
+<vaadin-button>
+  Button 
+</vaadin-button>
+`;
+  }
+
+  // Remove this method to render the contents of this view inside Shadow DOM
+  createRenderRoot() {
+    return this;
+  }
+}
