d994c3136d270fb4ec51ba461efac2221fdd5f07
Mateusz Łassa <59343944+Fenixx74540@users.noreply.github.com>
2022-05-16 16:05:12 +0200

    Podstawowa struktura projektu

---
+++ src/main/java/pl/edu/pbs/serwiskomputerowy/SerwisKomputerowyApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pbs.serwiskomputerowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerwisKomputerowyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerwisKomputerowyApplication.class, args);
+    }
+
+}
