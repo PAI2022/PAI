b6385eb96145cf34b49114eb34d077c3010a927a
Szymon <szymon.korpusinski@gmail.com>
2022-06-15 12:49:46 +0200

    Added filtering functionality for previously implemented buttons

--- src/main/java/pl/edu/pbs/serwiskomputerowy/ui/HomePage.java
+++ src/main/java/pl/edu/pbs/serwiskomputerowy/ui/HomePage.java
@@ -1,18 +1,26 @@
 package pl.edu.pbs.serwiskomputerowy.ui;
 
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.dependency.JsModule;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.littemplate.LitTemplate;
 import com.vaadin.flow.component.template.Id;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.renderer.LocalDateTimeRenderer;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 import pl.edu.pbs.serwiskomputerowy.model.Client;
 import pl.edu.pbs.serwiskomputerowy.model.Equipment;
 import pl.edu.pbs.serwiskomputerowy.service.ClientService;
 import pl.edu.pbs.serwiskomputerowy.service.EquipmentService;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * A Designer generated component for the home-page template.
@@ -24,16 +32,30 @@ import java.util.List;
 @JsModule("./home-page.ts")
 @Route("")
 public class HomePage extends LitTemplate {
+    @Id("nameCB")
+    private ComboBox<String> nameCB;
+    @Id("fixedCB")
+    private ComboBox<String> fixedCB;
+    @Id("resetFiltersBT")
+    private Button resetFiltersBT;
     @Id("equipmentGrid")
     private Grid<Equipment> equipmentGrid;
     @Id("clientGrid")
     private Grid<Client> clientGrid;
+    @Id("issuedDP")
+    private DatePicker issuedDP;
+    @Id("admissionDP")
+    private DatePicker admissionDP;
+    @Id("clientTF")
+    private TextField clientTF;
 
     private final EquipmentService equipmentService;
     private List<Equipment> equipmentList;
 
     private final ClientService clientService;
     private List<Client> clientList;
+
+    private Client pickedClient = null;
 
     /**
      * Creates a new HomePage.
@@ -67,11 +89,104 @@ public class HomePage extends LitTemplate {
         clientGrid.getColumns().forEach(col -> col.setAutoWidth(true));
 
         updateGrids();
+
+        nameCB.setItems(equipmentList.stream().map(Equipment::getEquipmentName).distinct().collect(Collectors.toList()));
+        fixedCB.setItems("Tak", "Nie");
+    }
+
+    @PostConstruct
+    private void init() {
+        resetFiltersBT.addClickListener(buttonClickEvent -> {
+            nameCB.setValue(null);
+            pickedClient = null;
+            fixedCB.setValue(null);
+            issuedDP.setValue(null);
+            admissionDP.setValue(null);
+            nameCB.setItems(equipmentList.stream().map(Equipment::getEquipmentName).distinct().collect(Collectors.toList()));
+            refreshEquipmentList();
+        });
+
+        nameCB.addValueChangeListener(value -> {
+            if (value != null) {
+                refreshEquipmentList();
+            }
+        });
+        fixedCB.addValueChangeListener(value -> {
+            if (value != null) {
+                refreshEquipmentList();
+            }
+        });
+        issuedDP.addValueChangeListener(value -> {
+            if (value != null) {
+                refreshEquipmentList();
+            }
+        });
+        admissionDP.addValueChangeListener(value -> {
+            if (value != null) {
+                refreshEquipmentList();
+            }
+        });
+
+        clientTF.addValueChangeListener(event -> updateClientList());
+        clientTF.setValueChangeMode(ValueChangeMode.LAZY);
+
+        clientGrid.asSingleSelect().addValueChangeListener(event -> handleClientSelect(event.getValue()));
+    }
+
+    private void handleClientSelect(Client value) {
+        if(value == null){
+            pickedClient = null;
+            nameCB.setItems(equipmentList.stream().map(Equipment::getEquipmentName).distinct().collect(Collectors.toList()));
+            refreshEquipmentList();
+        } else {
+            pickedClient = value;
+            nameCB.setItems(equipmentService.getAllEquipment().stream().filter(equipment -> Objects.equals(equipment.getClientID(), value.getClientID())).map(Equipment::getEquipmentName).distinct().collect(Collectors.toList()));
+            refreshEquipmentList();
+        }
+    }
+
+    private void updateClientList() {
+        clientList = clientService.getAllClients();
+        clientList = clientList.stream().filter(client -> client.getClientName().contains(clientTF.getValue())).collect(Collectors.toList());
+        updateGrids();
+    }
+
+    private void refreshEquipmentList(){
+        boolean isName = nameCB.getValue() != null;
+        boolean isClient = (pickedClient != null);
+        boolean isIssued = issuedDP.getValue() != null;
+        boolean isAdmissioned = admissionDP.getValue() != null;
+        boolean isFixed = Objects.equals(fixedCB.getValue(), "Tak");
+        boolean isFixedSelected = fixedCB.getValue() != null;
+
+        equipmentList = equipmentService.getAllEquipment();
+
+        if(isName){
+            equipmentList = equipmentList.stream().filter(equipment -> Objects.equals(equipment.getEquipmentName(), nameCB.getValue())).collect(Collectors.toList());
+        }
+        if(isClient){
+            equipmentList = equipmentList.stream().filter(equipment -> Objects.equals(equipment.getClientID(), pickedClient.getClientID())).collect(Collectors.toList());
+        }
+        if(isFixedSelected){
+            equipmentList = equipmentList.stream().filter(equipment -> Objects.equals(equipment.isEquipmentIsFixed(), isFixed)).collect(Collectors.toList());
+        }
+        if(isIssued){
+            equipmentList = equipmentList.stream().filter(equipment -> Objects.equals(equipment.getEquipmentIssueDate().toLocalDate(), issuedDP.getValue())).collect(Collectors.toList());
+        }
+        if(isAdmissioned){
+            equipmentList = equipmentList.stream().filter(equipment -> Objects.equals(equipment.getEquipmentAdmissionDate().toLocalDate(), admissionDP.getValue())).collect(Collectors.toList());
+        }
+
+        updateEquipmentGrid();
     }
 
     private void updateGrids(){
         equipmentGrid.setItems(equipmentList);
         clientGrid.setItems(clientList);
+    }
+
+    private void updateEquipmentGrid(){
+        equipmentGrid.setItems(equipmentList);
     }
 
 }
